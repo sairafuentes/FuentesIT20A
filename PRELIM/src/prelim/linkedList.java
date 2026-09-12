bd1e9520e4b4c2828bb03d141bf8943a253a81e8
Angela Andilan <nalidnaangela@gmail.com>
2026-09-12 21:00:18 +0800

    Added singlyLinkedList, arrayLinkedList, LinkedList, and doublyLinkedList

---
+++ PRELIM/src/prelim/linkedList.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prelim;
+
+import java.util.LinkedList;
+
+public class linkedList {
+     public static void main(String[] args){
+        LinkedList <Integer> num = new LinkedList<>();
+        num.add(1);
+        num.add(2);
+        
+        System.out.println(num.get(0));
+    
+}
+
+    
+}
